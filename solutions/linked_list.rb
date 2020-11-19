31564e0de893a0b1f8a827f9e9d8c9a88e82c0d8
Rupa <rupa211984@gmail.com>
2020-11-18 18:13:27 -0800

    Add ruby solution

--- solutions/linked_list.rb
+++ solutions/linked_list.rb
@@ -5,15 +5,12 @@ class LinkedList
     @head = head
   end
 
-  def iterate_with_count
+  def iterate
     count = 0
     temp = @head
 
     until temp.nil?
-      res = yield(temp, count)
-
-      return temp if res == true
-      
+      yield(temp, count)      
       temp = temp.next_node
       count += 1
     end
@@ -22,10 +19,7 @@ class LinkedList
   end
 
   def print
-    iterate do |node|
-      puts node.value
-      false
-    end
+    iterate { |node| puts node.value }
   end
 
   def find(target)
@@ -45,7 +39,7 @@ class LinkedList
     iterate do |curr_node|
       if curr_node.next_node.nil?
         curr_node.next_node = node
-        return true
+        return
       end
     end
   end
@@ -73,16 +67,12 @@ class LinkedList
       return node
     end
 
-    count = 0
-
-    iterate do |curr_node|
+    iterate do |curr_node, count|
       if count == idx - 1
         node.next_node = curr_node.next_node.next_node
         curr_node.next_node = node
         return node
       end
-
-      count += 1
     end
   end
 
@@ -92,17 +82,13 @@ class LinkedList
       return
     end
 
-    count = 0
-
-    iterate do |curr_node|
+    iterate do |curr_node, count|
       if count == idx - 1
         old_next = curr_node.next_node
         curr_node.next_node = node
         node.next_node = old_next
         return
       end
-
-      count += 1
     end
   end
 
@@ -111,16 +97,12 @@ class LinkedList
       return remove_first
     end
 
-    count = 0
-
-    iterate do |node|
+    iterate do |node, count|
       if count == idx - 1
         old_node = node.next_node
         node.next_node = node.next_node.next_node
         return old_node
       end
-
-      count += 1
     end
   end
 end
@@ -135,7 +117,79 @@ class Node
 end
 
 if __FILE__ == $PROGRAM_NAME
-  # Don't forget to add your tests!
+  head = Node.new('one', Node.new('two', Node.new('three', Node.new('four'))))
+  list = LinkedList.new(head)
+
+  puts "Print one to four"
+  list.print
+  puts "-----------------------------"
+
+  puts "Find four"
+  puts "#{list.find('four').value}"
+  puts "Find non-existent value"
+  puts "Nothing: #{list.find(50)}"
+  puts "-----------------------------"
+
+  puts "Add zero as head"
+  list.add_first(Node.new('zero'))
+  list.print
+  puts "-----------------------------"
+
+  puts "Add five as tail"
+  list.add_last(Node.new('five'))
+  list.print
+  puts "-----------------------------"
+
+  puts "Remove first node zero and return it"
+  puts "#{list.remove_first.value} was removed"
+  list.print
+  puts "-----------------------------"
+
+  puts "Remove last node five and return it"
+  puts "#{list.remove_last.value} was removed"
+  list.print
+  puts "-----------------------------"
+
+  puts "Replace node at index and return inserted node"
+  puts "replace middle two with 2: #{list.replace(1, Node.new('2')).value}"
+  list.print
+  puts "replace zeroth one with 1: #{list.replace(0, Node.new('1')).value}"
+  list.print
+  puts "replace tail four with 4: #{list.replace(3, Node.new('4')).value}"
+  list.print
+  puts "replace middle three with 3: #{list.replace(2, Node.new('3')).value}"
+  list.print
+  puts "-----------------------------"
+
+  puts "Insert node at index"
+  puts "Insert at 0"
+  list.insert(0, Node.new('zero'))
+  list.print
+  list.remove_first
+
+  puts "Insert at 2"
+  list.insert(2, Node.new('two'))
+  list.print
+
+  puts "Insert at 4"
+  list.insert(4, Node.new('four'))
+  list.print
+
+  puts "Insert at 6"
+  list.insert(6, Node.new('six'))
+  list.print
+  list.remove_last
+  puts "-----------------------------"
+
+  head = Node.new('one', Node.new('two', Node.new('three', Node.new('four'))))
+  list = LinkedList.new(head)
+
+  puts "Remove the node at the index and return it"
+  puts "Remove two: #{list.remove(1).value}"
+  puts "Remove tail four: #{list.remove(2).value}"
+  puts "Remove three: #{list.remove(1).value}"
+  puts "Remove one: #{list.remove(0).value}"
+  list.print
 end
 
 # Please add your pseudocode to this file
